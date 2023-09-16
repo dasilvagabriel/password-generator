122f09bdcf0e545eb35f8f774359daebb1f8f754
dasilvagabriel <gabriel1manoel@gmail.com>
2023-09-16 16:07:26 -0400

    added github button

---
+++ password-generator/src/components/GitHubButton.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Box from '@mui/material/Box';
+import { GitHub as GitHubIcon } from '@mui/icons-material';
+
+export default function GitHubButton() {
+    return (
+        <Box sx={{
+            position: 'fixed',
+            bottom: '10vh',
+            right: '5vw',
+            cursor: 'pointer',
+            zIndex: 10,
+            width: '140px',
+            height: '40px',
+            color: 'black',
+            backgroundColor: 'white',
+            fontSize: '12px',
+            fontFamily: 'Inter',
+            borderRadius: '20px',
+            padding: '8px',
+            textAlign: 'center',
+            boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)',
+            justifyContent: 'center',
+            display: 'flex', 
+            alignItems: 'center'
+        }}>
+            <a href="https://github.com/dasilvagabriel/password-generator" target="_blank" rel="noopener noreferrer" 
+               style={{textDecoration: 'none', display: 'flex', alignItems: 'center'}}>
+                <GitHubIcon sx={{ mr: 1 }} /> <b>Check the code</b>
+            </a>
+        </Box>
+    );
+}
