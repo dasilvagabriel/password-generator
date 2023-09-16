142d56cc0913df3b83f711244232d5d15d4cdee9
dasilvagabriel <gabriel1manoel@gmail.com>
2023-09-16 14:24:07 -0400

    final design

--- password-generator/src/App.tsx
+++ password-generator/src/App.tsx
@@ -8,11 +8,35 @@ function App() {
 
   return (
     <>
-      <div className='center-square'>
-        <p className='pass-title'>Generate a <br /> <span className='pass-green'>random password</span></p>
-        <p className="subtitle">Never use an insecure password again.</p>
-        <button className='btn'>Generate passwords</button> 
-        <hr />
+      <div className='double-square'>
+        <div className='center-square'>
+          <p className='pass-title'>Generate a <br /> <span className='pass-green'>random password</span></p>
+          <p className="subtitle">Never use an insecure password again.</p>
+          <button className='btn'>Generate passwords</button>
+          <hr />
+          <div className='input-boxes'>
+            <div className='input-box'>
+              <p className='pass-input'></p>
+            </div>
+            <div className='input-box'>
+              <p className='pass-input'></p>
+            </div>
+          </div>
+        </div>
+        <div className='center-square'>
+          <p className='pass-title'>Generate a <br /> <span className='pass-green'>random password</span></p>
+          <p className="subtitle">Never use an insecure password again.</p>
+          <button className='btn'>Generate passwords</button>
+          <hr />
+          <div className='input-boxes'>
+            <div className='input-box'>
+              <p className='pass-input'></p>
+            </div>
+            <div className='input-box'>
+              <p className='pass-input'></p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
